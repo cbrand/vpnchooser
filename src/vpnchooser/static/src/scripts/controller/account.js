5acaa47f5e5db09eae41e0bf98fd00ce4aa5db37
Christoph Brand <ch.brand@scolterius.net>
2014-11-02 21:13:27 +0100

    [Frontend: Controllers] Adjusted some timing functionality in the account controller.

--- src/vpnchooser/static/src/scripts/controller/account.js
+++ src/vpnchooser/static/src/scripts/controller/account.js
@@ -1,4 +1,4 @@
-vpnChooserControllers.controller('accountCtrl', function ($scope, UserService) {
+vpnChooserControllers.controller('accountCtrl', function ($scope, $timeout, UserService) {
 
     $scope.user = UserService;
 
@@ -9,20 +9,34 @@ vpnChooserControllers.controller('accountCtrl', function ($scope, UserService) {
             newPassword = $scope.newPassword
         ;
 
-
+        ['message', 'error'].forEach(function(name) {
+            $scope.message &&
+            $scope.message[name] &&
+            $timeout.cancel($scope.message[name]);
+        });
+        $scope.message = null;
+        $scope.error = null;
         UserService.changePassword(oldPassword, newPassword)
             .success(function() {
                 $scope.message = {
                     header: 'Password changed',
-                    text: 'Your password has been changed successfully.'
-                }
+                    text: 'Your password has been changed successfully.',
+                    timeout: $timeout(function() {
+                        $scope.message = null;
+                    }, 5000)
+                };
+                $scope.oldPassword = '';
+                $scope.newPassword = '';
             })
             .catch(function() {
                 $scope.error = {
                     header: 'Password change failed',
                     text: 'Could not change your password. Please verify ' +
-                        'if your old one has been entered correctly.'
-                }
+                        'if your old one has been entered correctly.',
+                    timeout: $timeout(function() {
+                        $scope.error = null;
+                    }, 5000)
+                };
             })
         ;
         return false;
