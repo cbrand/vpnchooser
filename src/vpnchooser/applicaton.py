4d27c6470a463a7efa692301bc45b8943749e958
Christoph Brand <ch.brand@scolterius.net>
2015-04-04 20:21:02 +0200

    [Application] Root URL now redirects to the static index template.

--- src/vpnchooser/applicaton.py
+++ src/vpnchooser/applicaton.py
@@ -1,8 +1,14 @@
 # -*- encoding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask.ext.restful import Api
 
 
 app = Flask('vpnchooser')
 api = Api(app)
+
+@app.route('/')
+@app.route('/static')
+@app.route('/static/')
+def index_route():
+    return redirect(url_for('static', filename='index.html'))
